c10c8879075224ac8a34c054a865196d199abeea
Peter Dorglo <paddypyker@gmail.com>
2025-02-14 20:30:49 +0000

    fix: build command

--- src/test/kotlin/com/ai/bible/quotation_app/AiBibleQuotationAppApplicationTests.kt
+++ src/test/kotlin/com/ai/bible/quotation_app/AiBibleQuotationAppApplicationTests.kt
@@ -3,7 +3,7 @@ package com.ai.bible.quotation_app
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AiBibleQuotationAppApplicationTests {
 
     @Test
